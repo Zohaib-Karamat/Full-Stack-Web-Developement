1ab247ca6e5b2668e896dabc4f9eec362a8773b9
Zohaib Karamat <juttzohaib875@gmail.com>
2026-03-10 09:24:47 +0500

    react basics

---
+++ REACT/First Project/first_project/src/Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+    const food1 = "Banana"
+    const food2 = "Apple"
+
+    return(
+        <ul>
+            <li>Mango</li>
+            <li>{food1.toUpperCase()}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+
+
+export default Food
---
+++ REACT/First Project/first_project/src/HeaderNew.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return(
+        <div>
+        <header>
+            <h2>MY WEBSITE</h2>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Contact Us</li>
+                <li>Products</li>
+                <li>About Us</li>
+            </ul>
+        </header>
+        <hr />
+        </div>
+    );
+}
+
+export default Header;
