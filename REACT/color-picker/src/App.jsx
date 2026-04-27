750aaedfd8df3c96e44d28e13c9864199a595ee0
Zohaib Karamat <juttzohaib875@gmail.com>
2026-04-27 12:14:34 +0500

    changes

--- REACT/color-picker/src/App.jsx
+++ REACT/color-picker/src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css"
 import { ColorPicker } from "./ColorPicker"
+import { BrowserRouter, Routes, Route, Link } from "react-router";
 
 function App() {
-
+  
+  
   return (
     <>
-      <ColorPicker/>
+      <BrowserRouter>
+      <div>
+        
+        <Link to="/">Home</Link>
+        <Link to="/color-picker">Color-Picker</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to Color-Picker App</h1>}/>
+        <Route path="/color-picker" element={<ColorPicker/>} />
+      </Routes>
+      </BrowserRouter>
+      
     </>
   )
 }
