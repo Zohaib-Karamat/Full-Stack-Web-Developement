79c347e8fd02af7b7b50fbdcc7a4de063733368c
Zohaib Karamat <juttzohaib875@gmail.com>
2026-05-11 16:23:17 +0500

    Node practice started with fs system

---
+++ Node/first-project/index.js
@@ -0,0 +1,81 @@
+import fs from "fs/promises";
+
+// READ FILE
+async function readFileData() {
+  try {
+    const data = await fs.readFile("./files/test.txt", "utf-8");
+
+    console.log("File Content:");
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// WRITE FILE
+async function writeFileData() {
+  try {
+    await fs.writeFile(
+      "./files/write.txt",
+      "This file is created by Node.js"
+    );
+
+    console.log("write.txt created successfully");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// APPEND FILE
+async function appendFileData() {
+  try {
+    await fs.appendFile(
+      "./files/write.txt",
+      "\nNew line added using appendFile"
+    );
+
+    console.log("Content appended");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// RENAME FILE
+async function renameFileData() {
+  try {
+    await fs.rename(
+      "./files/write.txt",
+      "./files/newWrite.txt"
+    );
+
+    console.log("File renamed");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// DELETE FILE
+async function deleteFileData() {
+  try {
+    await fs.unlink("./files/newWrite.txt");
+
+    console.log("File deleted");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+// RUN ALL FUNCTIONS
+async function start() {
+  await readFileData();
+
+  await writeFileData();
+
+  await appendFileData();
+
+  await renameFileData();
+
+  await deleteFileData();
+}
+
+start();
