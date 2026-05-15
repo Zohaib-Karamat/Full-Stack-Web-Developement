085abbdb901dec8ce8b2e842dc212370b33387b4
Zohaib Karamat <juttzohaib875@gmail.com>
2026-05-15 14:46:11 +0500

    new change

--- JS/eventloop.js
+++ JS/eventloop.js
@@ -1,25 +1,42 @@
 
+// console.log("Hi!");
+
+// setTimeout(function newtimeout() {
+//     console.log("Click the button!");
+// }, 5000);
+
+// console.log("Welcome to loupe.");
+
+// let pr = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         let status = true;
+//     if(status){
+//         resolve("Chal gaya kam");
+//     }
+//     else{
+//         reject("nahi chala kam");
+//     }
+//     },5000);
+    
+// });
+
+// pr.then((data)=>console.log(data)).catch((err)=>{console.log(err)});
+
+
 console.log("Hi!");
 
-setTimeout(function newtimeout() {
-    console.log("Click the button!");
-}, 5000);
+ function fetchData() {
 
-console.log("Welcome to loupe.");
+ setTimeout(function newtimeout() {
+         console.log("Click the button!");
+    }, 5000);
+}
+const main = async () => {
+    await fetchData();
+}
+main();
+console.log("Welcome to loupe.")
 
-let pr = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        let status = true;
-    if(status){
-        resolve("Chal gaya kam");
-    }
-    else{
-        reject("nahi chala kam");
-    }
-    },5000);
-    
-});
 
-pr.then((data)=>console.log(data)).catch((err)=>{console.log(err)});
 
 
